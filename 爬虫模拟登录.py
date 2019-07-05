4ab1dad702eed567ac286dac48652d5b2bd172e4
rongweihe <rongweihe1995@gmail.com>
2019-07-05 23:04:38 +0800

    Update 爬虫模拟登录.py

--- 爬虫模拟登录.py
+++ 爬虫模拟登录.py
@@ -18,3 +18,24 @@ cookies = wd.get_cookies() #导出cookie
 for cookie in cookies:
     req.cookies.set(cookie['name'],cookie['value']) #转换cookies
 print(cookies) #打印cookies
+
+
+# encoding=utf8 
+'''
+解决中文乱码 第一个 python 爬虫
+'''
+import sys  
+import requests
+from selenium import webdriver
+chromePath = r'B:\Download\chromedriver.exe'
+wd = webdriver.Chrome(executable_path= chromePath) #构建浏览器
+loginUrl = 'http://bestcoder.hdu.edu.cn/login.php' 
+wd.get(loginUrl) #进入登陆界面
+wd.find_element_by_xpath('//*[@id="username"]').send_keys('username') #输入用户名
+wd.find_element_by_xpath('//*[@id="password"]').send_keys('passwd') #输入密码
+wd.find_element_by_xpath('//*[@id="login-form"]/form/fieldset/div[2]/button').click() #点击登陆
+req = requests.Session() #构建Session
+cookies = wd.get_cookies() #导出cookie
+for cookie in cookies:
+    req.cookies.set(cookie['name'],cookie['value']) #转换cookies
+print(cookies) #打印cookies
