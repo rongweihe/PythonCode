1f443dee7c365119b6fff95d9d16a43dcd3853c2
rongweihe <rongweihe1995@gmail.com>
2021-06-26 11:51:56 +0800

    add WebSpiderSeed

---
+++ 爬虫代码/WebSpiderSeed/main.py
@@ -0,0 +1,17 @@
+'''
+Author: herongwei
+Date: 2021-06-26 11:06:09
+LastEditTime: 2021-06-26 11:11:31
+LastEditors: Please set LastEditors
+Description: main函数
+FilePath: /WebSpiderSeed/main.py
+'''
+#!/usr/bin/spython
+# -*- coding: utf-8 -*-
+
+from lib.config_helper import ConfigHelper
+from lib.io_helper import IOHelper
+from lib.log_helper import LogHelper
+from lib.req_helper import ReqHelper
+from bin.app_search_all import do_search
+
