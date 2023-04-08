e2f2124fcaae01ccc777b62c0ffea93a5aed6330
程序员贺同学 <rongweihe1995@126.com>
2023-04-08 21:14:58 +0800

    Update 爬虫模拟登录.py

--- 项目实战/爬虫模拟登录.py
+++ 项目实战/爬虫模拟登录.py
@@ -10,7 +10,7 @@ chromePath = r'B:\Download\chromedriver.exe'
 wd = webdriver.Chrome(executable_path= chromePath) #构建浏览器
 loginUrl = 'http://www.weibo.com/login.php' 
 wd.get(loginUrl) #进入登陆界面
-wd.find_element_by_xpath('//*[@id="loginname"]').send_keys('18800119432') #输入用户名
+wd.find_element_by_xpath('//*[@id="loginname"]').send_keys('xxxx') #输入用户名
 wd.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[2]/div/input').send_keys('weibo.com') #输入密码
 wd.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[6]/a').click() #点击登陆
 req = requests.Session() #构建Session
